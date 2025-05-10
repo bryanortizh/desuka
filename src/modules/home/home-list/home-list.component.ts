091fc15199d35dfa2d6905a7e38524c01a4485c8
bryanortizh <davidmarlon1924@hotmail.com>
2025-05-09 23:49:34 -0500

    Add capacitor voice speech recognition for search box

--- src/modules/home/home-list/home-list.component.ts
+++ src/modules/home/home-list/home-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { SpeechRecognition } from '@capacitor-community/speech-recognition';
 
 @Component({
   selector: 'app-home-list',
@@ -8,7 +9,6 @@ import { FormBuilder, FormGroup } from '@angular/forms';
   standalone: false,
 })
 export class HomeListComponent implements OnInit {
-
   searchForm!: FormGroup;
 
 
@@ -26,6 +26,34 @@ export class HomeListComponent implements OnInit {
     console.log('Searching for:', searchValue);
   }
 
-  openVoiceSearch() {
+  async openVoiceSearch() {
+    const available = await SpeechRecognition.available();
+
+    if (!available) {
+      console.error('Speech recognition no está disponible en este dispositivo.');
+      return;
+    }
+
+    const hasPermission = await SpeechRecognition.checkPermissions();
+    console.log("hasPermission", hasPermission)
+    if (!hasPermission.speechRecognition) {
+      await SpeechRecognition.requestPermissions();
+    }
+    SpeechRecognition.start({
+      language: 'es-ES',
+      maxResults: 1, 
+      prompt: 'Habla ahora...', 
+      partialResults: true,
+      popup: true,
+    }).then((result) => {
+      if (result.matches && result.matches.length > 0) {
+        const spokenText = result.matches[0];
+        console.log('Texto detectado:', spokenText);
+
+        this.searchForm.patchValue({ search: spokenText });
+      }
+    }).catch((error) => {
+      console.error('Error en el reconocimiento de voz:', error);
+    });
   }
 }
